da90dbe3f30736987ec1bbe37cc0b8e94d203f8e
Irina Serova <isserova@gmail.com>
2018-12-28 17:17:45 -0800

    first commit

---
+++ GuessGame.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class GuessGame {
+
+	public static void main(String[] args) {
+		// get ready to supply input data 
+		Scanner scan = new Scanner(System.in);
+		
+		// create a random number from 1 - 100
+		int randomNumber = (int)(Math.random()*100 + 1);
+		int guess = 0;
+		// start while loop for guessing
+		while(guess != randomNumber) {
+			System.out.println("Guess a number between 1 and 100: ");
+			// get the user's number
+			guess = scan.nextInt();
+			
+			// compare user's number to randomNumber
+			
+			if (guess < randomNumber)
+				//ask player to play again
+				System.out.println(" Your Number " + guess + " is too low. Please try again.");				
+			else if(guess > randomNumber)
+				//ask player to play again
+				System.out.println(" Your Number " + guess + " is too high. Please try again.");
+			else 
+				// congratulations
+				System.out.println(" Your Number " + guess + " is correct. Congratulations! You Won!");			
+		} // end of while loop for comparing
+			
+		
+
+	}
+
+}
